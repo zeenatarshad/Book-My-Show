eb39d87585dc5cc1d540cc3044ccc7198abfad2a
zeenatarshad <zputhawala3011@gmail.com>
2021-10-14 19:50:00 +0530

    bms commit

---
+++ planning.js
@@ -0,0 +1,23 @@
+// Book My Show Planning
+
+//  -> We need a Reusable Carousel
+//  -> Hero Carousel
+//  -> Common Carousel
+//  -> Cast & Crew Small Carousel
+
+
+// Pages
+// -> Home Page
+// -> Movie Page
+// -> Categories Page
+// -> Auth Page
+// -> Razorpay Payment Gateway
+
+// Plan -> How are we building it??
+// Layouts
+// Routes
+// Pages
+// Components
+
+// HOC
+// Higher Order Components                                                                                  
